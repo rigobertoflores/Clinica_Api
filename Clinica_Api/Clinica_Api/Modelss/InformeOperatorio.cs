619b5935ecec6dc82db365bb6dc38565f761d031
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2024-05-01 14:14:50 -0500

    first commit to production

---
+++ Clinica_Api/Clinica_Api/Modelss/InformeOperatorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clinica_Api.Modelss;
+
+public partial class InformeOperatorio
+    {
+        public int Id { get; set; }
+        public string? Informe { get; set; }
+        public string?  Nombre { get; set;}
+    }
+
