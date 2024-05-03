388bfcb12372467f5e2258da5fc0f60f0a69bbbf
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2024-05-02 19:43:06 -0500

    swagger

--- Clinica_Api/Clinica_Api/Program.cs
+++ Clinica_Api/Clinica_Api/Program.cs
@@ -21,7 +21,10 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 // Agrega servicios al contenedor.
 builder.Services.AddDbContext<DbOliveraClinicaContext>(options =>
@@ -41,7 +44,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
 }
 
 app.UseHttpsRedirection();
