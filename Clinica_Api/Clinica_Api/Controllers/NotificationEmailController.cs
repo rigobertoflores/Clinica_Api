a8515e840dc6a529e1a943547ac951183156dd4d
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2024-05-16 21:06:16 -0500

    ENVIO DE CORREOS

--- Clinica_Api/Clinica_Api/Controllers/NotificationEmailController.cs
+++ Clinica_Api/Clinica_Api/Controllers/NotificationEmailController.cs
@@ -1,6 +1,7 @@
 ﻿using Clinica_Api.Modelss;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
 using System.Numerics;
 
 namespace Clinica_Api.Controllers
@@ -62,39 +63,6 @@ namespace Clinica_Api.Controllers
         }
 
 
-        //[HttpPost("NotificationEmailController/EditPlantilla")]
-
-        //public IActionResult Edit([FromBody] Plantillas plantilla)
-        //{
-        //    Plantillas plantillaModificada = null;
-        //    try
-        //    {
-        //        var context = _context.Plantillas.ToList();
-
-        //        if (context == null || !ModelState.IsValid)
-        //        {
-        //            return NotFound();
-        //        }
-        //        plantillaModificada = new Plantillas()
-        //        {
-        //            Nombre = plantilla.Nombre,
-        //            Asunto = plantilla.Asunto,
-        //            CuerpoEmail = plantilla.CuerpoEmail,
-        //            FechaEnvio = plantilla.FechaEnvio,
-        //            Adjunto = ""
-        //        };
-        //        _context.Plantillas.Update(plantillaModificada);
-        //        _context.SaveChanges();
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Maneja cualquier error que pueda ocurrir durante el guardado.
-        //        return StatusCode(500, "No se pudo guardar la información del paciente. Error: " + ex.Message);
-        //    }
-
-        //    return Ok(plantillaModificada);
-        //}
 
 
         [HttpDelete("NotificationEmailController/DeletePlantilla/{id:int}")]
@@ -108,6 +76,35 @@ namespace Clinica_Api.Controllers
                 _context.Plantillas.Remove(plantillaAEliminar);
             _context.SaveChanges();
             return Ok(plantillaAEliminar);
+        }
+
+
+
+        [HttpGet("NotificationEmailController/SendEmail")]
+        public IActionResult SendEmail(int id)
+        {
+            string emailOrigen = "developertestkat@gmail.com";
+            string pass = "K@therine92";
+            string emailDestino = "developertestkat@gmail.com";
+            string correoTexto = "este es un correo de prueba";
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(emailOrigen, "AMECAE");
+            mailMessage.To.Add(new MailAddress(emailDestino, "Yo"));
+            mailMessage.Body = correoTexto;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Subject = "prueba";
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new System.Net.NetworkCredential(emailOrigen, pass);
+            smtpClient.Send(mailMessage);
+            smtpClient.Dispose();
+
+            var plantillaAEliminar = _context.Plantillas.Where(t => t.Id == id).First();
+
+            return Ok("Correo enviado con exito");
         }
     }
 }
