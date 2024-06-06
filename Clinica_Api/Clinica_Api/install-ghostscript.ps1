eba5141e0c3c8a9f7854eb865ddffb4a18c9b05c
rigobertoflores <93140192+rigobertoflores@users.noreply.github.com>
2024-06-05 20:16:20 -0500

    Ghostscript

---
+++ Clinica_Api/Clinica_Api/install-ghostscript.ps1
@@ -0,0 +1,11 @@
+﻿# install - ghostscript.ps1
+$ghostscriptUrl = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs950/gs950w64.exe"
+$installerPath = "$env:HOME\site\wwwroot\gs950w64.exe"
+Invoke - WebRequest - Uri $ghostscriptUrl - OutFile $installerPath
+Start - Process - FilePath $installerPath - ArgumentList "/S /D=$env:ProgramFiles\gs" - NoNewWindow - Wait
+
+# Add Ghostscript to the PATH
+$envPath = [System.Environment]:: GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]:: Machine)
+if (-not($envPath - like "*$env:ProgramFiles\gs\gs9.50\bin*")) {
+    [System.Environment]:: SetEnvironmentVariable('PATH', "$envPath;$env:ProgramFiles\gs\gs9.50\bin", [System.EnvironmentVariableTarget]:: Machine)
+}
